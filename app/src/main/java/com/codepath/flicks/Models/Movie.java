b1bb9752f1d0ba0f2050cc50bc668a93de351b3b
Maya Jeyendran <mayajey@fb.com>
2017-06-22 15:30:03 -0700

    intermittent youtube

--- app/src/main/java/com/codepath/flicks/Models/Movie.java
+++ app/src/main/java/com/codepath/flicks/Models/Movie.java
@@ -1,8 +1,17 @@
 package com.codepath.flicks.Models;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
+
+import cz.msebera.android.httpclient.Header;
+
+import static com.codepath.flicks.MovieListActivity.API_KEY_PARAM;
 
 /**
  * Created by mayajey on 6/21/17.
@@ -19,6 +28,12 @@ public class Movie {
     String backdropPath;
     // average rating of a movie
     Double voteAverage;
+    // video ID
+    Integer id;
+    // Client for API
+    AsyncHttpClient client;
+    // URL of the current movie's video
+    String url = "https://api.themoviedb.org/3/movie/";
 
     // no-arg, empty constructor required for Parceler
     public Movie() {}
@@ -30,6 +45,8 @@ public class Movie {
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
         voteAverage = object.getDouble("vote_average");
+        id = object.getInt("id");
+        url = url + id.toString() + "/";
     }
 
     // Getters
@@ -51,5 +68,34 @@ public class Movie {
 
     public Double getVoteAverage() {
         return voteAverage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    // API call to youtube ID (first ID if existing)
+    public void getVideo() {
+        // Set request parameters
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, "787f5f4b449efac476d72f5401f14664");
+        // Make GET request; override methods in handler
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    // load the movie's youtube id and make the url
+                    JSONArray results = response.getJSONArray("results");
+                    // extract first one if existing
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                //
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/codepath/flicks/MovieTrailerActivity.java
@@ -0,0 +1,54 @@
+package com.codepath.flicks;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+
+        // temporary test video id -- TODO replace with movie trailer video id
+        final String videoId = "tKodtNFpzBA";
+
+        // resolve the player view from the layout
+        YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
+
+        // initialize with API key stored in secrets.xml
+        playerView.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(videoId);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                YouTubeInitializationResult youTubeInitializationResult) {
+                // log the error
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player");
+            }
+        });
+    }
+
+    // alert user to errors
+    private void logError(String message, Throwable error, boolean alertUser) {
+        // first log the error
+        Log.e("MOVIE_TRAILER_ACTIVITY", message, error);
+        // alert user if necessary
+        if (alertUser) {
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+}
